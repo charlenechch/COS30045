c7452c65d94f541a933726b2a600b3e964352686
charlenechch <102781873@students.swinburne.edu.my>
2024-11-16 21:40:57 +0800

    D3 for First Visualisation

--- vis1.js
+++ vis1.js
@@ -1,11 +1,10 @@
 var w = 1300; // Width
-var h = 600; // Height
+var h = 650; // Height
 var padding = 70;
 
 var dataset, xScale, yScale, line;
 
 function init() {
-    console.log("Initializing visualization...");
 
     // Load the CSV data
     d3.csv("resource/Historical data global.csv", function (d) {
@@ -19,21 +18,19 @@ function init() {
             return d.date.getFullYear() >= 1850;
         });
 
-        console.log("Filtered Dataset:", dataset);
-
         // Set up scales
         xScale = d3.scaleTime()
-            .domain(d3.extent(dataset, function (d) { return d.date; })) // Scale based on filtered data
-            .range([padding, w - padding]);
+                   .domain(d3.extent(dataset, function (d) { return d.date; })) // Scale based on filtered data
+                   .range([padding, w - padding]);
 
         yScale = d3.scaleLinear()
-            .domain([0, d3.max(dataset, function (d) { return d.emissions; })])
-            .range([h - padding, padding]);
+                   .domain([0, d3.max(dataset, function (d) { return d.emissions; })])
+                   .range([h - padding, padding]);
 
         // Define the line generator
         line = d3.line()
-            .x(function (d) { return xScale(d.date); })
-            .y(function (d) { return yScale(d.emissions); });
+                 .x(function (d) { return xScale(d.date); })
+                 .y(function (d) { return yScale(d.emissions); });
 
         // Render the chart
         lineChart(dataset);
@@ -41,24 +38,22 @@ function init() {
 }
 
 function lineChart(dataset) {
-    console.log("Rendering line chart...");
-
     // Remove existing visualization if present
     d3.select("#chart").select("svg").remove();
 
     // Create SVG
     var svg = d3.select("#chart")
-        .append("svg")
-        .attr("width", w)
-        .attr("height", h);
+                .append("svg")
+                .attr("width", w)
+                .attr("height", h);
 
     // Draw the line
     var path = svg.append("path")
-        .datum(dataset)
-        .attr("fill", "none")
-        .attr("stroke", "steelblue")
-        .attr("stroke-width", 2)
-        .attr("d", line);
+                  .datum(dataset)
+                  .attr("fill", "none")
+                  .attr("stroke", "steelblue")
+                  .attr("stroke-width", 2)
+                  .attr("d", line);
 
     // Animate the line drawing
     var totalLength = path.node().getTotalLength();
@@ -66,14 +61,14 @@ function lineChart(dataset) {
     path.attr("stroke-dasharray", totalLength + " " + totalLength)
         .attr("stroke-dashoffset", totalLength)
         .transition()
-        .duration(2000) // Animation duration
+        .duration(2500) // Animation duration
         .ease(d3.easeLinear)
         .attr("stroke-dashoffset", 0);
 
     // Add x-axis
     var xAxis = d3.axisBottom(xScale)
-        .ticks(10)
-        .tickFormat(d3.timeFormat("%Y")); // Format years
+                  .ticks(10)
+                  .tickFormat(d3.timeFormat("%Y")); // Format years
 
     svg.append("g")
         .attr("transform", "translate(0," + (h - padding) + ")")
@@ -89,8 +84,8 @@ function lineChart(dataset) {
 
     // Add y-axis
     var yAxis = d3.axisLeft(yScale)
-        .ticks(10)
-        .tickFormat(d => `${d}B t`); // Format in billion tons
+                  .ticks(10)
+                  .tickFormat(d => `${d} billion t`); // Format in billion tons
 
     svg.append("g")
         .attr("transform", "translate(" + padding + ",0)")
@@ -102,7 +97,7 @@ function lineChart(dataset) {
         .attr("transform", "rotate(-90)") // Rotate to make it vertical
         .attr("x", -h / 2)
         .attr("y", 20) // Position to the left of the y-axis
-        .text("Emissions (Billion Tons)")
+        .text("Total Greenhouse Gas Emissions")
         .style("font-size", "16px");
 
     // Create a tooltip
@@ -126,7 +121,7 @@ function lineChart(dataset) {
         .attr("class", "dot")
         .attr("cx", function (d) { return xScale(d.date); })
         .attr("cy", function (d) { return yScale(d.emissions); })
-        .attr("r", 5)
+        .attr("r", 2)
         .attr("fill", "steelblue")
         .style("opacity", 0) // Initially hidden
         .on("mouseover", function (event, d) {
