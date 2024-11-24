4017ff34ec8d27ff47e407bfef0f3c1407e3bec2
charlenechch <102781873@students.swinburne.edu.my>
2024-11-24 16:51:43 +0800

    Third Visualisation

--- vis3.js
+++ vis3.js
@@ -2,9 +2,9 @@
 // var w = 800; // Chart width
 // var h = 700; // Chart height
 // var padding = 50;
-// var currentYear = "2008"; // Default year
-// var outerRadius = w / 3.2;  // Increased radius to give space for labels
-// var innerRadius = 0;
+var currentYear = "2008"; // Default year
+var outerRadius = w / 3.2;  // Increased radius to give space for labels
+var innerRadius = 0;
 
 var dataset3, pie, arc, color;
 
